525928c964c9749b5b7b49d7ecb1789876fe4217
TylorDev <jhontmg26@gmail.com>
2024-03-21 16:45:10 -0300

    hero terminado!

--- src/Pages/Home/Sections/Hero.jsx
+++ src/Pages/Home/Sections/Hero.jsx
@@ -1,9 +1,37 @@
 import "./Hero.scss";
+import { GoDotFill } from "react-icons/go";
+import { FaFacebookF } from "react-icons/fa";
+import { SlSocialVkontakte } from "react-icons/sl";
 import { Button } from "../../../Components";
+import { IoLogoTwitter } from "react-icons/io";
 function Hero() {
   return (
-    <section className="">
-      <Button name="PLAY ADVANCED"></Button>
+    <section className="Hero">
+      <div className="scroll ">
+        <GoDotFill className=" icon current" size={15}></GoDotFill>
+        <GoDotFill className="icon" size={15}></GoDotFill>
+        <GoDotFill className="icon" size={15}></GoDotFill>
+        <GoDotFill className="icon" size={15}></GoDotFill>
+      </div>
+      <div className=" hero-content ">
+        <h1>Forge your firepower</h1>
+        <h2>
+          ALL-NEW G604 FOR BATTLE ROYALE MASTERY. FOCUS YOUR SKILL AND OWN EVERY
+          MOMENT OF THE FIGHT.
+        </h2>
+        <p>
+          Make your weapons play harder in the configuration you deem fittest.
+          Use the 15 programmable controls, LIGHTSPEED or Bluetooth® wireless,
+          HERO 16K sensor, and 240 hour battery life to be the fastest
+          gunslinger on the battlefield.
+        </p>
+        <Button name="PLAY ADVANCED"></Button>
+      </div>
+      <div className="scroll-r  ">
+        <FaFacebookF className="icon" size={15}></FaFacebookF>
+        <IoLogoTwitter className="icon" size={15}></IoLogoTwitter>
+        <SlSocialVkontakte className="icon" size={15}></SlSocialVkontakte>
+      </div>
     </section>
   );
 }
--- src/Pages/NavBar.jsx
+++ src/Pages/NavBar.jsx
@@ -1,5 +1,36 @@
-function NavBar() {
-  return <div>navbar</div>;
-}
+import "./Navbar.scss";
 
-export default NavBar;
+const Navbar = () => {
+  return (
+    <nav className="navbar ">
+      <div className="navbar-logo  ">
+        {/* Aquí puedes colocar tu logo */}
+        <img src="Logo.svg" alt="Logo" />
+      </div>
+      <ul className="navbar-nav ">
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            Product
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            Specifications
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
